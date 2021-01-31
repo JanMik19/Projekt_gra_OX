83953f0df713cea8966048174fc17d76a0cb5ca5
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-31 13:17:12 +0100

    Update Czlowiek.cpp
    komentarze

--- Czlowiek.cpp
+++ Czlowiek.cpp
@@ -1,27 +1,27 @@
 #include "Czlowiek.h"
 
-Czlowiek::Czlowiek() {}
+Czlowiek::Czlowiek() {} // kostruktor domyslny
 
-Czlowiek::Czlowiek(char symbol) : Gracz(symbol) {}
+Czlowiek::Czlowiek(char symbol) : Gracz(symbol) {} // kosntruktor parametryczny
 
 int Czlowiek::get_ruch(Kolko_Krzyzyk& obj)
 {
 	int index;
 	bool ruch = false;
 
-	while (!ruch)
+	while (!ruch) // petla - jezeli mozna dokonac ruch to wykona sie petla
 	{
 		try
 		{
 			std::cout << std::endl << "Kolej gracza " << get_symbol() << ": ";
 			std::cin >> index;
 			if (obj.wstaw_znak(index - 1) != ' ')
-				throw "Pozycja zajeta!";
+				throw "Pozycja zajeta!"; // rzucenie wyjatkiem
 			ruch = true;
 		}
 		catch (const char err[])
 		{
-			std::cout << std::endl << err;
+			std::cout << std::endl << err; // lapanie wyjatku 
 		}
 	}
 	return index - 1;

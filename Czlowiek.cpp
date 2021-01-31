56a45abbc8123aaeb43b0307cdc38fbe87c139ce
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-31 17:08:43 +0100

    Update Czlowiek.cpp
    popr

--- Czlowiek.cpp
+++ Czlowiek.cpp
@@ -9,20 +9,27 @@ int Czlowiek::get_ruch(Kolko_Krzyzyk& obj)
 	int index;
 	bool ruch = false;
 
-	while (!ruch) // petla - jezeli mozna dokonac ruch to wykona sie petla
+	do // petla - jezeli mozna dokonac ruch to wykona sie petla
 	{
+		std::cout << std::endl << "Kolej gracza " << get_symbol() << ": ";
+		std::cin >> index;
+		
 		try
 		{
-			std::cout << std::endl << "Kolej gracza " << get_symbol() << ": ";
-			std::cin >> index;
-			if (obj.wstaw_znak(index - 1) != ' ')
+			if (!cin.good())
+			{
+				system("cls");
+				std::cout << std::endl << "Zly znak wstawiono!!! Koniec programu";
+				exit(1);
+			}
+			else if (obj.wstaw_znak(index - 1) != ' ')
 				throw "Pozycja zajeta!"; // rzucenie wyjatkiem
 			ruch = true;
 		}
-		catch (const char err[])
+		catch (const char a[])
 		{
-			std::cout << std::endl << err; // lapanie wyjatku 
+			std::cout << std::endl << a; // lapanie wyjatku 
 		}
-	}
+	} while (!ruch && cin.good());
 	return index - 1;
 }

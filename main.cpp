007e2956833b61d920f81c6c5d64ff87a6068bbb
JanMik19 <44546286+JanMik19@users.noreply.github.com>
2021-01-31 13:24:52 +0100

    Update main.cpp
    komentarze

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 
 int main()
 {
-	Kolko_Krzyzyk gra;
-	AI wsk;
+	Kolko_Krzyzyk gra; // tworzyny objekt na klase Kolko_Krzyzyk, czyli inicjalizujemy to, co sie tam znajduje (tablice itd...)
+	AI wsk; // wczesniej byl wskaznik stad ta nazwa "wsk", ale jest teraz objekt, mozna zamienic na wspaznik i wtedy funkcje "zacznij_gre()" wywolac operatorem "->"
 	char kolejnosc, decyzja;
 
-	gra.instrukcja();
+	gra.instrukcja(); // wyswietlenie wstepu i instrukcji
 	
 	do
 	{
 		std::cout << "Czy chcesz zaczac jako pierwszy ? (t/n) :";
 		std::cin >> kolejnosc;
 
-		if (kolejnosc == 't' || kolejnosc == 'T')
-			wsk.zacznij_gre(gra, new Czlowiek('X'), new AI('O'));
-		else
-			wsk.zacznij_gre(gra, new AI('X'), new Czlowiek('O'));
+		if (kolejnosc == 't' || kolejnosc == 'T') // jezeli zaczynamy pierwsi, to czlowiek ma 'X', komputer 'O'
+			wsk.zacznij_gre(gra, new Czlowiek('X'), new AI('O')); // zawolanie poprzez objekt klasy "AI"
+		else // inaczej to komputer zaczyna jako 'X'
+			wsk.zacznij_gre(gra, new AI('X'), new Czlowiek('O')); // zawolanie poprzez objekt klasy "AI", jak wyzej napisano, moze byc tez zawolane popzez wskaznik
 
 		std::cout << "\nChcesz zagracz ponownie? (t/n) : ";
 		std::cin >> decyzja;
